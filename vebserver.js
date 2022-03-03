103e09d3fd6a7246af89b3e046297f7fa5a12cab
thousandminusseven <makar.sankov2013@yandex.ru>
2022-03-03 12:44:55 +0300

    marschutizatir

--- vebserver.js
+++ vebserver.js
@@ -1,6 +1,7 @@
 const http = require('http')
 const port = 3000;
 const hostname = '127.0.0.1'; 
+
 const server = http.createServer ((request, response) => { 
   response.statusCode =100;
   response.setHeader('Content-Type', 'text/plain');
@@ -9,3 +10,21 @@ const server = http.createServer ((request, response) => {
     
 server.listen (port, hostname, () =>{console.log (`servser running at http://${hostname}:${port}/` );
 });
+const requestHandler = (request, response) => {
+  response.setHeader("Content-Type", "text/html; charset=utf-8;");
+  if(request.url === "/home" || request.url === "/"){
+  response.write("<h2>Home</h2>");
+  }
+  else if(request.url == "/about"){
+  response.write("<h2>About</h2>");
+  }
+  else if(request.url == "/contact"){
+  response.write("<h2>Contacts</h2>");
+  }
+  else{
+  response.write("<h2>Not found</h2>");
+  }
+  response.end();
+  };
+  http.createServer(requestHandler).listen(3000);
+  
