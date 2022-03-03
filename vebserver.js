0c610cb3869a3c5e2a1fed5322e522ca0501600d
thousandminusseven <makar.sankov2013@yandex.ru>
2022-03-03 09:57:03 +0300

    создание веб сервера

--- vebserver.js
+++ vebserver.js
@@ -1,3 +1,10 @@
 const http = require('http')
-
+const port = 3000;
+const hostname = '127.0.0.1'; 
+const server = http.createServer ((request, response) => { 
+  response.statusCode =100;
+  response.setHeader('Content-Type', 'text/plain');
+  response.end('Hellow World\n');
+});
+    
 http.createserver().listen(3000)
