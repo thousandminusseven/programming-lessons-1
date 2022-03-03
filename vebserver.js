0fd44d65678b23f49a45521d3454f63a1276d69f
thousandminusseven <makar.sankov2013@yandex.ru>
2022-03-03 16:45:59 +0300

    Добавляет систему логирования

--- vebserver.js
+++ vebserver.js
@@ -1,25 +1,29 @@
 const http = require('http')
+
 const port = 3000;
 const hostname = '192.168.1.159'; 
 
 const server = http.createServer ((request, response) => { 
   response.statusCode =200;
   response.setHeader('Content-Type', 'text/plain');
-  response.end('Hellow World\n');
+
 });
     
 server.listen (port, hostname, () =>{console.log (`servser running at http://${hostname}:${port}/` );
 });
 
+
 const requestHandler = (request, response) => {
   response.setHeader("Content-Type", "text/html; charset=utf-8;");
-  if(request.url === "/home" || request.url === "/"){
+  if (request.url === "/home" || request.url === "/"){
   response.write("<h2>Home</h2>");
+  fs.appendFileSync("new.txt",'home');
   }
-  else if(request.url == "/about"){
+  else if (request.url == "/about"){
   response.write("<h2>About</h2>");
+  fs.appendFileSync("new.txt",'home');
   }
-  else if(request.url == "/contact"){
+  else if (request.url == "/contact"){
   response.write("<h2>Contacts</h2>");
   }
   else{
@@ -27,6 +31,16 @@ const requestHandler = (request, response) => {
   }
   response.end();
   };
+  const fs = require("fs");
+fs.appendFileSync("new.txt", "Привет!");
+fs.appendFile("new.txt", "Привет М!", function(error){
+if(error) throw error; 
+console.log("Запись файла завершена. Содержимое файла:");
+let data = fs.readFileSync("new.txt", "utf8");
+console.log(data); 
+});
+
+  
   http.createServer(requestHandler).listen(3000);
   
 
