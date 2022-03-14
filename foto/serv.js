2b1d133b48ae4e28dcd269855a54432b2e386429
thousandminusseven <makar.sankov2013@yandex.ru>
2022-03-14 12:12:27 +0300

    15

--- foto/serv.js
+++ foto/serv.js
@@ -1,7 +1,5 @@
 const fs = require("fs");
 const http = require("http");
-
-
 const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     if (request.url === "/home" || request.url === "/") {
