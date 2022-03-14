8c602340b1b0df9edef67268f3fea65ed97d7a5c
thousandminusseven <makar.sankov2013@yandex.ru>
2022-03-14 11:03:38 +0300

    12

---
+++ foto/serv.js
@@ -0,0 +1,19 @@
+const fs = require("fs");
+const http = require("http");
+
+
+const requestHandler = (request, response) => {
+    response.setHeader("Content-Type", "text/html; charset=utf-8;");
+    if (request.url === "/home" || request.url === "/") {
+        response.write("<h2>hello world</h2>");
+        fs.appendFileSync("hello.txt", 'переход в hello\n');
+    } else if (request.url == "/about") {
+        response.write("<h2>About</h2>");
+    } else if (request.url == "/about") {
+        response.write("<h2>About</h2>");
+
+    } else if (request.url == "/image/1") {
+        let content = fs.readFileSync("1.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        });
