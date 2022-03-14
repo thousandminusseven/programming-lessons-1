a110141d5e2f2af3a0faa39a9b525e3000d03650
thousandminusseven <makar.sankov2013@yandex.ru>
2022-03-14 11:15:03 +0300

    14

--- foto/serv.js
+++ foto/serv.js
@@ -6,7 +6,7 @@ const requestHandler = (request, response) => {
     response.setHeader("Content-Type", "text/html; charset=utf-8;");
     if (request.url === "/home" || request.url === "/") {
         response.write("<h2>hello world</h2>");
-        fs.appendFileSync("hello.txt", 'переход в hello\n');
+        fs.appendFileSync("new.txt", 'переход в hello\n');
     } else if (request.url == "/about") {
         response.write("<h2>About</h2>");
     } else if (request.url == "/about") {
@@ -24,3 +24,39 @@ const requestHandler = (request, response) => {
             'Content-Type': 'image/jpeg'
         })
         response.end(content);
+    } else if (request.url == "/image/3") {
+        let content = fs.readFileSync("3.jpg");
+        response.writeHead(200, {
+            'Content-Type': 'image/jpeg'
+        })
+        response.end(content);
+} else if (request.url == "/image/4") {
+    let content = fs.readFileSync("4.jpg");
+    response.writeHead(200, {
+        'Content-Type': 'image/jpeg'
+    })
+    response.end(content);
+} else if (request.url == "/image/5") {
+    let content = fs.readFileSync("5.jpg");
+    response.writeHead(200, {
+        'Content-Type': 'image/jpeg'
+    })
+    response.end(content);
+} else if (request.url == "/image/6") {
+    let content = fs.readFileSync("6.jpg");
+    response.writeHead(200, {
+        'Content-Type': 'image/jpeg'
+    })
+    response.end(content);
+} else if (request.url == "/image/7") {
+    let content = fs.readFileSync("7.jpg");
+    response.writeHead(200, {
+        'Content-Type': 'image/jpeg'
+    })
+    response.end(content);
+}
+
+
+};
+
+http.createServer(requestHandler).listen(3000); 
